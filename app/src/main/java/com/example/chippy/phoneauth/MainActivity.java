b75cfc46624adb7af12fd65dd461c5bffa487b2b
athiraivan <athiramary15@gmail.com>
2018-04-17 15:50:38 +0530

    test

--- app/src/main/java/com/example/chippy/phoneauth/MainActivity.java
+++ app/src/main/java/com/example/chippy/phoneauth/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 
         import android.util.Log;
+        //test
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
--- app/src/main/java/com/example/chippy/phoneauth/main.java
+++ app/src/main/java/com/example/chippy/phoneauth/main.java
@@ -15,6 +15,7 @@ public class main extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //test
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
